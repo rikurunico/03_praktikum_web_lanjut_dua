5dffbcb395014f267e1cb811edbe308ab84b3504
nicoco1173 <rikurunico@gmail.com>
2022-03-03 16:36:35 +0700

    Praktikum 2 First Commit

---
+++ resources/views/blog/video.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+@include('partisi.headContent')
+</head>
+
+<body>
+    <div class="tm-page-wrap mx-auto">
+        <div class="position-relative">
+            @include('partisi.header')
+            @include('blog.contentvideo')
+            @include('partisi.footer')
+            </div>
+        </div>
+    </div>
+    <script>
+        @include('partisi.script')
+    </script>
+</body>
+
+</html>
